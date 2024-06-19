b66ee292a83665582fdea759137495909b63f2a3
Foga Kater Amos <katfogy@gmail.com>
2024-06-19 11:47:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ let image = document.getElementById('imgSource');
 let Imagesize = document.getElementById('Imagesize');
 let Text = document.getElementById('inputText');
 const api = "https://chart.googleapis.com/chart?chs";
-// const api = "https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=hello%20bro";
 
 btn.addEventListener('click', () => {
-  if (Text.value == "") {
-    alert('Enter Text to Generate Qr Code');
+  if (Text.value === "") {
+    alert('Enter Text to Generate QR Code');
   } else {
-    image.src =`${api}=${Imagesize.value}x${Imagesize.value}&cht=qr&chl=${Text.value}`;
+    image.src = `${api}=${Imagesize.value}x${Imagesize.value}&cht=qr&chl=${encodeURIComponent(Text.value)}`;
   }
 });
